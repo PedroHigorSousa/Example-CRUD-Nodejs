8afd00430ef315e29935b82b47536dd917c4a85e
PedroHigorSousa <higorwork.sousa@hotmail.com>
2021-02-15 15:27:04 -0300

    Working with Middlewares

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const { request, response } = require('express')
 const express = require('express')
-
 const server = express()
 
 server.use(express.json())
 
 const users = ['Higor', 'Rafael', 'Leonardo', 'Lucas', 'Gilberto']
+
+// Middleware Global
+server.use((request, response, next) => {
+    console.log(`Method: ${request.method}; URL: ${request.url};`)
+
+    return next();
+})
+
+// Middlewares
+const Middlewares = {
+    checkUserExists(request, response, next) {
+        const user = users[request.body.index]
+
+        if (!request.body.name) {
+            return response.status(400).json(
+                {
+                    error: 'User name is required'
+                }
+            )
+        }
+
+        return next()
+    },
+
+    checkUserInArray(request, response, next) {
+        const user = users[request.params.index]
+
+        if (!users[request.params.index]) {
+            return response.status(400).json(
+                {
+                    error: 'User does not exists'
+                }
+            )
+        }
+
+        request.user = user
+
+        return next()
+    }
+}
 
 // All Users
 server.get('/users/all', (request, response) => {
@@ -13,25 +51,13 @@ server.get('/users/all', (request, response) => {
 })
 
 // Only Users
-server.get('/users/:index', (request, response) => {
-    const { index } = request.params
-
-    response.json(users[index])
+server.get('/users/:index', Middlewares.checkUserInArray, (request, response) => {
+    response.json(request.user)
 })
 
 // Create User
-server.post('/users/new', (request, response) => {
+server.post('/users/new', Middlewares.checkUserExists, (request, response) => {
     const { name } = request.body
-
-    if (name !== undefined)
-        users.push(name)
-    else {
-        return response.json(
-            {
-                message: "The necessary data has not been passed"
-            }
-        )
-    }
 
     return response.json(
         {
@@ -41,7 +67,7 @@ server.post('/users/new', (request, response) => {
 })
 
 // Update User
-server.put('/users/update/:index', (request, response) => {
+server.put('/users/update/:index', Middlewares.checkUserInArray, Middlewares.checkUserExists, (request, response) => {
     const { index } = request.params
     const { name } = request.body
 
@@ -57,7 +83,7 @@ server.put('/users/update/:index', (request, response) => {
 })
 
 // Delete User
-server.delete('/users/remove/:index', (request, response) => {
+server.delete('/users/remove/:index', Middlewares.checkUserInArray, (request, response) => {
     const { index } = request.params
     const user = users[index]
 
